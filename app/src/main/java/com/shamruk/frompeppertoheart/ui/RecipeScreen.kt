ecc1d908fc86e048187c9df28607d71ac6850908
Yura <yura.shamruk@gmail.com>
2018-03-24 23:04:43 +0300

    temp commit

---
+++ app/src/main/java/com/shamruk/frompeppertoheart/ui/RecipeScreen.kt
@@ -0,0 +1,12 @@
+package com.shamruk.frompeppertoheart.ui
+
+class RecipeScreen : MainActivityFragment() {
+
+    class Constanst{
+        companion object {
+            const val TAG = "RecipeScreen"
+        }
+    }
+
+    
+}
