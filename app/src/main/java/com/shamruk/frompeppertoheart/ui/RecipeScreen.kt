5a5617190492d13fd433a61f4cb4f935344d85fa
Yura <yura.shamruk@gmail.com>
2018-03-24 23:06:47 +0300

    added: log2

--- app/src/main/java/com/shamruk/frompeppertoheart/ui/RecipeScreen.kt
+++ app/src/main/java/com/shamruk/frompeppertoheart/ui/RecipeScreen.kt
@@ -1,5 +1,9 @@
 package com.shamruk.frompeppertoheart.ui
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
 class RecipeScreen : MainActivityFragment() {
 
     class Constanst{
@@ -8,5 +12,8 @@ class RecipeScreen : MainActivityFragment() {
         }
     }
 
-    
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(Constanst.TAG, "2")
+    }
 }
