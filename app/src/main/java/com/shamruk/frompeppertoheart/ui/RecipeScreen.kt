d35f7e8d6fc0533258b560eac540fa331c6a8c1c
Yura <yura.shamruk@gmail.com>
2018-03-24 23:07:54 +0300

    added: TAG2

--- app/src/main/java/com/shamruk/frompeppertoheart/ui/RecipeScreen.kt
+++ app/src/main/java/com/shamruk/frompeppertoheart/ui/RecipeScreen.kt
@@ -5,6 +5,7 @@ class RecipeScreen : MainActivityFragment() {
     class Constanst{
         companion object {
             const val TAG = "RecipeScreen"
+            const val TAG2 = "RecipeScreen2"
         }
     }
 
